1584965cd61a098e5fc92a5e7b7e70d79275ab54
Andres Calderon <andress.calderon@gmail.com>
2026-03-16 10:51:28 -0500

    Adding Class 08 and Lab 07 in DBA...

---
+++ 2026-10/DBA/Labs/Lab07/resources/euroleagues/Euroleagues_copy.sql
@@ -0,0 +1,6 @@
+COPY countries FROM '/opt/Datasets/Euroleagues/countries.tsv';
+COPY leagues FROM '/opt/Datasets/Euroleagues/leagues.tsv';
+COPY players FROM '/opt/Datasets/Euroleagues/players.tsv';
+COPY player_attributes FROM '/opt/Datasets/Euroleagues/player_attributes.tsv';
+COPY teams FROM '/opt/Datasets/Euroleagues/teams.tsv';
+COPY matches FROM '/opt/Datasets/Euroleagues/matches.tsv';
