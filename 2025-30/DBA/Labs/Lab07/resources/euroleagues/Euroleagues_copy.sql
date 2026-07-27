92fc454e5ea6a2704e82af906e866ea2b673a5de
Andres Calderon <andress.calderon@gmail.com>
2026-07-27 10:28:36 -0500

    Adding 2026-30...

--- 2025-30/DBA/Labs/Lab07/resources/euroleagues/Euroleagues_copy.sql
+++
@@ -1,6 +0,0 @@
-COPY countries FROM '/opt/Datasets/Euroleagues/countries.tsv';
-COPY leagues FROM '/opt/Datasets/Euroleagues/leagues.tsv';
-COPY players FROM '/opt/Datasets/Euroleagues/players.tsv';
-COPY player_attributes FROM '/opt/Datasets/Euroleagues/player_attributes.tsv';
-COPY teams FROM '/opt/Datasets/Euroleagues/teams.tsv';
-COPY matches FROM '/opt/Datasets/Euroleagues/matches.tsv';
